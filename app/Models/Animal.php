904a4a79844cd200b9a5db396d112e1dccf058ee
Noemi Muñiz Gamero <noemipracticasdaw@gmail.com>
2026-03-26 19:38:35 +0100

    Modelo animal

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -7,4 +7,42 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     //
+
+  protected $table = 'animales';
+
+    protected $fillable = [
+        'nombre',
+        'especie',
+        'raza',
+        'edad',
+        'descripcion',
+        'foto',
+        'refugio_id',
+        'estado',
+    ];
+    //RELACIONES
+    //Cada animal pertenece a un refugio
+    public function refugio()
+    {
+        return $this->belongsTo(User::class, 'refugio_id');
+    }
+
+    //Un animal puede tener muchas solicitudes
+    public function solicitudes()
+    {
+        return $this->hasMany(Solicitud::class, 'animal_id');
+    }
+
+    //Para saber si está disponible la adopción
+    public function disponible()
+    {
+        return $this->estado === 'disponible';
+    }
+
+    // ¿Ya ha sido adoptado?
+    public function adoptado()
+    {
+        return $this->estado === 'adoptado';
+    }
+
 }
